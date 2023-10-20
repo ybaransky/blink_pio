5b326aaaad4d426a9adb54c008faf432c01ece00
ybaransky <ybaransky@gmail.com>
2023-10-19 20:59:57 -0400

    added blink()

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #define PL(x) Serial.println(x)
 #define PV(x) P(#x); P("="); PL(x)
 
-void print_env(void) {
+void print_environment(void) {
   P("compile time:  "); P(__DATE__); P(" " );  PL(__TIME__);
   PV(LED_BUILTIN);
 }
@@ -18,20 +18,35 @@ void setup() {
   digitalWrite(LED_BUILTIN,LOW); // turn it off
 
   delay(2000);
-  PL();
-  PL("starting...");
-  print_env();
+  PL(); PL("starting...");
+  print_environment();
 }
 
+void blink(uint n) {
+  static int count=0;
+  count = count%n;
+  // only blink if we are actuall off
+  if (digitalRead(LED_BUILTIN))
+    return;
+
+  count++;
+  PV(count);
+  // blink this count times, each time more and more
+  for (uint i=0; i < count; i++) {
+    digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+    delay(100);
+    digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+    delay(100);
+  } 
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
   uint t = millis();
-  P("time: "); PL(t);
-  delay(1000);
-  digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+  P("time: "); P(t); P(" "); PL(digitalRead(LED_BUILTIN));
 
-  print_env();
-  PV(digitalRead(LED_BUILTIN));
+  delay(1000);
+  blink(5);
+//  digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
 }
 
